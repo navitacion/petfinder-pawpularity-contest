3dcf413c5f1fd1a2cce5c4f4ce75695a7dbd94e5
navitacion <masataka.i.navi@gmail.com>
2021-11-16 08:43:53 +0900

    refactoring

---
+++ src/model/build_model.py
@@ -0,0 +1,16 @@
+
+from src.model.cnn import PetFinderModel
+from src.model.hybrid_swin_cnn import PawpularityHybridModel
+
+def get_model(cfg, logger=None):
+
+    if cfg.model.type == 'cnn':
+        net = PetFinderModel(**dict(cfg.cnn_model))
+        logger.log_hyperparams(dict(cfg.cnn_model))
+    elif cfg.model.type == 'hybrid':
+        net = PawpularityHybridModel(**dict(cfg.hybrid_model))
+        logger.log_hyperparams(dict(cfg.hybrid_model))
+    else:
+        net = None
+
+    return net
