4c81cd5d116ec29841b0b5e21a7c4e68806b2bdc
navitacion <masataka.i.navi@gmail.com>
2021-11-16 20:45:45 +0900

    refactoring

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -10,19 +10,9 @@ class CSVDataLoader:
     def __init__(self, cfg):
         self.cfg = cfg
 
-
     def _get_fold(self, df):
         df['fold'] = -1
         df['Target_ceil'] = pd.qcut(df['Pawpularity'].values, 5, labels=np.arange(5))
-        df['Target_is_100'] = df['Pawpularity'].apply(lambda x: 1 if x >= 100 else 0)
-
-
-        if self.cfg.data.target_type == 'regression':
-            y_col = 'Target_ceil'
-        elif self.cfg.data.target_type == 'classification':
-            y_col = 'Target_is_100'
-        else:
-            y_col = None
 
         # StratifiedKFold
         # binning by Pawpularity
@@ -31,13 +21,12 @@ class CSVDataLoader:
             shuffle=True,
             random_state=self.cfg.data.seed
         )
-        for i, (trn_idx, val_idx) in enumerate(kf.split(df, df[y_col].values)):
+        for i, (trn_idx, val_idx) in enumerate(kf.split(df, df['Target_ceil'].values)):
             df.loc[val_idx, 'fold'] = i
 
         df['fold'] = df['fold'].astype(np.float16)
 
         return df
-
 
     def get_data(self):
         data_dir = Path(self.cfg.data.data_dir)
@@ -49,6 +38,10 @@ class CSVDataLoader:
 
         train['is_train'] = 1
         test['is_train'] = 0
+
+        # ある閾値を超えるかどうかの分類問題として解く
+        if self.cfg.data.target_type == 'classification':
+            train['Pawpularity'] = train['Pawpularity'].apply(lambda x: 100 if x > self.cfg.data.cls_th else 0)
 
         df = pd.concat([train, test], axis=0, ignore_index=True)
 
