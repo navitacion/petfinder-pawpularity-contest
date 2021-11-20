ac5a57711f6ad837af2c3e799766066e4c8db2d9
masat <masataka.i.navi@gmail.com>
2021-11-20 12:59:06 +0900

    refactoring

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -8,22 +8,18 @@ from src.constant import TABULAR_FEATURES
 LEN_TABULAR_FEATURES = len(TABULAR_FEATURES)
 
 class LinearReluLayer(nn.Module):
-    def __init__(self, in_features, hidden_size, layer_num=3):
+    def __init__(self, hidden_size, layer_num=3, dropout_rate=0.5):
         super(LinearReluLayer, self).__init__()
 
         layers = []
         for i in range(layer_num):
-            if i == 0:
-                layers.append(nn.Linear(in_features, hidden_size, bias=False))
-            else:
-                layers.append(nn.Linear(hidden_size, hidden_size, bias=False))
-
             layers.append(nn.BatchNorm1d(hidden_size))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(nn.Linear(hidden_size, hidden_size, bias=False))
 
-            if i == layer_num - 1:
-                pass
-            else:
-                layers.append(nn.ReLU(inplace=True))
+        layers = layers[:-1]
+        layers.append(nn.Dropout(dropout_rate))
+        layers.append(nn.Linear(hidden_size, hidden_size, bias=False))
 
         self.layer = nn.ModuleList(layers)
 
@@ -33,42 +29,31 @@ class LinearReluLayer(nn.Module):
         return x
 
 class TabularModel(nn.Module):
-    def __init__(self, num_features, hidden_size):
+    def __init__(self, num_features, hidden_size, dropout_rate, num_unit=4):
         super(TabularModel, self).__init__()
 
-        self.start = nn.Sequential(
-            nn.Linear(num_features, hidden_size, bias=False),
-            nn.BatchNorm1d(hidden_size)
-        )
+        self.start = nn.Linear(num_features, hidden_size, bias=False)
 
-        self.head1 = LinearReluLayer(hidden_size, hidden_size)
-        self.head2 = LinearReluLayer(hidden_size, hidden_size)
-        self.head3 = LinearReluLayer(hidden_size, hidden_size)
-        self.head4 = LinearReluLayer(hidden_size, hidden_size)
+        self.head = nn.ModuleList([
+            LinearReluLayer(hidden_size, dropout_rate=dropout_rate) for _ in range(num_unit)
+        ])
 
     def forward(self, x):
-        x1 = self.start(x)
+        x = self.start(x)
+        skip_x = x
 
-        x2 = self.head1(x1)
-        x2 = nn.ReLU()(x2 + x1)
+        for l in self.head:
+            x = l(x) + skip_x
+            skip_x = x
 
-        x3 = self.head2(x2)
-        x3 = nn.ReLU()(x3 + x2)
-
-        x4 = self.head3(x3)
-        x4 = nn.ReLU()(x4 + x3)
-
-        x5 = self.head4(x4)
-        x5 = nn.ReLU()(x5 + x4)
-
-        return x5
+        return x
 
 
 class PetFinderModel(nn.Module):
     def __init__(self, backbone, pretrained=True, out_dim=1, hidden_size=256, dropout_rate=0.2):
         super(PetFinderModel, self).__init__()
         self.img_layer = create_model(backbone, pretrained=pretrained, num_classes=0)
-        self.tabular_layer = TabularModel(LEN_TABULAR_FEATURES, hidden_size)
+        self.tabular_layer = TabularModel(LEN_TABULAR_FEATURES, hidden_size, dropout_rate)
 
         self.head = nn.Sequential(
             nn.Dropout(dropout_rate),
@@ -88,11 +73,14 @@ class PetFinderModel(nn.Module):
 
 
 if __name__ == '__main__':
-    z = torch.randn(4, 3, 380, 380)
-    tabular = torch.ones(4, 12)
+    # z = torch.randn(4, 3, 380, 380)
+    # tabular = torch.ones(4, 12)
+    #
+    # net = PetFinderModel(backbone='tf_efficientnet_b4_ns', pretrained=False, out_dim=1)
+    #
+    # out = net(z, tabular)
+    # print(out.size())
 
-    net = PetFinderModel(backbone='tf_efficientnet_b4_ns', pretrained=False, out_dim=1)
-
-    out = net(z, tabular)
-    print(out.size())
+    a = [1,233,45,6,32]
+    print(a[:-1])
 
